626062c10f89e984f7e1ae384984b405b1dc6c05
Alex Poloz <alexypoloz@gmail.com>
2023-09-06 21:14:00 +0300

    Work version

---
+++ scripts/main.py
@@ -0,0 +1,9 @@
+from lib.s3 import s3, upload_file, get_policy
+
+
+if __name__ == '__main__':
+    for bucket in s3.buckets.all():
+        print(bucket.name)
+        print(get_policy(bucket.name))
+
+    print(upload_file(input("Файл: "), 'test'))
